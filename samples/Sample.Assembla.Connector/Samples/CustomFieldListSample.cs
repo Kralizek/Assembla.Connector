d21a4ee10345c91c7e3be9611a24318fc8471343
elin.danielsson <elin.danielsson@educations.com>
2019-11-14 14:51:11 +0100

    Fix for custom fields of list type
    - Fixed serialization issue of list options (array=>commaseparated)
    - Added possibility for adding required custom fields without a default value
    - Added sample for testing list type custom fields
    - Added tests for new JsonConverter
    - Added assembla api error message to error log

---
+++ samples/Sample.Assembla.Connector/Samples/CustomFieldListSample.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kralizek.Assembla;
+using Kralizek.Assembla.Connector.Spaces.Tools;
+using Kralizek.Assembla.Connector.Tickets;
+using Kralizek.Assembla.Connector.Tickets.CustomFields;
+using Shouldly;
+
+namespace Sample.Assembla.Connector.Samples
+{
+    public class CustomFieldListSample : ISample
+    {
+        public async Task Execute(IAssemblaClient client)
+        {
+            using (var test = await client.CreateDisposableSpace(requiredTools: ToolType.Tickets))
+            {
+                var customField = await client.Tickets.CustomFields.CreateAsync(test.Space.WikiName,
+                    new CustomField
+                    {
+                        Title = "Type", 
+                        IsRequired = true, 
+                        Type = CustomFieldType.List,
+                        ListOptions = new[] {"Research", "Development", "Maintenance"},
+                    });
+
+                var ticketWithRequiredCustomField = await client.Tickets.CreateAsync(test.Space.WikiName,
+                    new Ticket
+                    {
+                        Summary = "Ticket with required custom field",
+                        CustomFields = new Dictionary<string, string>() {{"Type", "Development"}}
+                    });
+
+                ticketWithRequiredCustomField.CustomFields[customField.Title].ShouldBe("Development");
+
+                var customFieldwithDefault = await client.Tickets.CustomFields.CreateAsync(test.Space.WikiName,
+                    new CustomField
+                    {
+                        Title = "TypeDefault",
+                        IsRequired = true,
+                        Type = CustomFieldType.List,
+                        ListOptions = new[] { "Research", "Development", "Maintenance" },
+                        DefaultValue = "Maintenance"
+                    });
+
+                var ticketWithDefaultCustomField = await client.Tickets.CreateAsync(test.Space.WikiName,
+                    new Ticket
+                    {
+                        Summary = "Ticket with required custom field",
+                        CustomFields = new Dictionary<string, string>() { { "Type", "Development" } }
+                    });
+
+                ticketWithDefaultCustomField.CustomFields[customField.Title].ShouldBe("Development");
+                ticketWithDefaultCustomField.CustomFields[customFieldwithDefault.Title].ShouldBe("Maintenance");
+
+                var allCustomFields = await client.Tickets.CustomFields.GetAllAsync(test.Space.WikiName);
+
+                allCustomFields.ShouldContain(c => c.Id == customField.Id);
+            }
+        }
+    }
+}
---
+++ tests/Tests.Assembla.Connector/Connector/Tickets/CustomFields/ListStringJsonConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using Kralizek.Assembla.Connector.Tickets.CustomFields;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Tests.Connector.Tickets.CustomFields
+{
+    [TestFixture]
+    public class ListStringJsonConverterTests
+    {
+        [Test, CustomAutoData]
+        public void String_arrays_can_be_deserialized(string[] strings)
+        {
+            var json = $"{{\"Items\":[\"{string.Join("\",\"", strings)}\"]}}";
+
+            var deserialized = JsonConvert.DeserializeObject<TestType>(json);
+
+            Assert.That(deserialized.Items, Is.EquivalentTo(strings));
+        }
+
+        [Test,CustomAutoData]
+        public void String_array_is_serialized_to_comma_separated_string(TestType item)
+        {
+            var expected = $"{{\"Items\":\"{string.Join(",", item.Items)}\"}}";
+
+            var serialized = JsonConvert.SerializeObject(item);
+
+            Assert.That(serialized, Is.EqualTo(expected));
+        }
+
+        public class TestType
+        {
+            [JsonConverter(typeof(ListStringJsonConverter))]
+            public string[] Items { get; set; }
+        }
+    }
+}
